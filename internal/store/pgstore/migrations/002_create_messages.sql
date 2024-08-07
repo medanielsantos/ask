d41ca728025f2ce4ae37d73abfe4f1974c3504a0
Daniel Henrique <medanielsantos@gmail.com>
2024-08-06 23:03:38 -0300

    create migration ask

--- internal/store/pgstore/migrations/002_create_messages.sql
+++ internal/store/pgstore/migrations/002_create_messages.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS messages (
-  "id" uuid PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
-  "room_id" uuid NOT NULL,
-  "message" VARCHAR(255) NOT NULL,
-  "reaction_count" BIGINT NOT NULL DEFAULT 0,
-  "aswered" BOOLEAN NOT NULL DEFAULT FALSE,
+    "id"                uuid            PRIMARY KEY     NOT NULL    DEFAULT gen_random_uuid(),
+    "room_id"           uuid                            NOT NULL,
+    "message"           VARCHAR(255)                    NOT NULL,
+    "reaction_count"    BIGINT                          NOT NULL    DEFAULT 0,
+    "answered"          BOOLEAN                         NOT NULL    DEFAULT false,
 
-  FOREIGN KEY (room_id) REFERENCES rooms (id) ON DELETE CASCADE,
+    FOREIGN KEY (room_id) REFERENCES rooms(id)
 );
 
 ---- create above / drop below ----
