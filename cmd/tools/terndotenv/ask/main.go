c4eb91c521bab8b9c055aaee537677268d448ac2
Daniel Henrique <medanielsantos@gmail.com>
2024-08-06 23:48:17 -0300

    feat: create server

---
+++ cmd/tools/terndotenv/ask/main.go
@@ -0,0 +1,53 @@
+package ask
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+	"github.com/medanielsantos/ask/internal/store/pgstore"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	pool, err := pgxpool.New(context.Background(), fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	))
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer pool.Close()
+
+	if err := pool.Ping(ctx); err != nil {
+		panic(err)
+	}
+
+	handler := api.newHandler(pgstore.New(pool))
+
+	go func() {
+		if err := http.ListenAndServe(":8080", handler); err != nil {
+			if !errors.Is(err, http.ErrServerClosed) {
+				panic(err)
+			}
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+
+	signal.Notify(quit, os.Interrupt)
+}
